046eab022e1aa8e2393ba897179cc85f0a54ba00
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2020-06-03 19:42:45 +0200

    Update Catalina ID
    Resolves #21

--- unraid.sh
+++ unraid.sh
@@ -164,7 +164,7 @@ fi
 			echo "I am going to download the Catalina recovery media. Please be patient!"
 		    echo "."
 		    echo "."
-	    "/Macinabox/tools/FetchMacOS/fetch.sh" -p 061-70033 -c PublicRelease || exit 1;
+	    "/Macinabox/tools/FetchMacOS/fetch.sh" -p 001-04366 -c PublicRelease || exit 1;
 	else
 		echo "Media already exists. I have already downloaded the Catalina install media before"
 	    echo "."
