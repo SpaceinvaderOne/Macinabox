8810c73e3f02a2526af472d8b8d4ca3db90e3616
SpaceinvaderOne <ed@Mac-Pro.local>
2021-04-04 21:16:33 +0100

    typo

--- tools/FetchMacOS/fetch.sh
+++ tools/FetchMacOS/fetch.sh
@@ -7,8 +7,7 @@ cd "$SCRIPTDIR" || exit
 # Method 1 or 2
 if [ "$method" = "method 1" ] ; then		
 # method 1 uses script2
-
-python3 fetch-macos.py2 
+python3 fetch-macos2.py 
 # method 2 uses script 1 
 elif [ "$method" = "method 2" ] ; then
 python3 fetch-macos.py "$@"
