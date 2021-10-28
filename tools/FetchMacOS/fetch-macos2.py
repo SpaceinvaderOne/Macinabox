cdbd730f52d5bcd1d269533dbf5417b328fa763f
82ghost82 <crudo.daniele@gmail.com>
2021-10-28 09:06:53 +0200

    Fix board id for Big Sur

--- tools/FetchMacOS/fetch-macos2.py
+++ tools/FetchMacOS/fetch-macos2.py
@@ -479,7 +479,7 @@ def main():
             {"name": "Mojave (10.14)", "b": "Mac-7BA5B2DFE22DDD8C", "m": "00000000000KXPG00"},
             {"name": "Catalina (10.15) - RECOMMENDED", "b": "Mac-00BE6ED71E35EB86", "m": "00000000000000000"},
             # {"name": "Latest (Big Sur - 11)", "b": "Mac-E43C1C25D4880AD6", "m": "00000000000000000", "os_type": "latest"}
-            {"name": "Latest (Big Sur - 11)", "b": "Mac-E43C1C25D4880AD6", "m": "00000000000000000", "os_type": "default"}
+            {"name": "Latest (Big Sur - 11)", "b": "Mac-2BD1B31983FE1663", "m": "00000000000000000", "os_type": "default"}
             ]
 
     for index, product in enumerate(products):
